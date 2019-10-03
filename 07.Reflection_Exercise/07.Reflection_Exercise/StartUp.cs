5608a743d488150ef942fc92ece3a54d8705d079
Николай Дойчев <nikolay.doichev@gmail.com>
2019-10-03 22:26:15 +0300

    07.Reflection_Exercise

---
+++ 07.Reflection_Exercise/07.Reflection_Exercise/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.Reflection_Exercise
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
