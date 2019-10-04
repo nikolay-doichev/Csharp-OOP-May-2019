38604a975337147264bd29eb91054e213ff9e1c9
Николай Дойчев <nikolay.doichev@gmail.com>
2019-10-04 20:20:38 +0300

    07.Reflection_Lecture

---
+++ 07.Reflection_Lecture/06CodeTracker/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+[Author("Ventsi")]
+class StartUp
+{
+    [Author("Gosho")]
+    static void Main(string[] args)
+    {
+        var tp = typeof(StartUp);
+
+        var atributes = tp.CustomAttributes;
+
+        foreach (var attr in atributes)
+        {
+            if (attr.AttributeType == typeof(AuthorAttribute))
+            {
+                var name = attr.ConstructorArguments.FirstOrDefault().Value;
+                System.Console.WriteLine(name);
+            }
+        }
+        var methods = tp.GetMethods(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+
+        foreach (var method in methods)
+        {
+            var attributesMethods = method.CustomAttributes;
+
+            foreach (var attr in attributesMethods)
+            {
+                if (attr.AttributeType == typeof(AuthorAttribute))
+                {
+                    var name = attr.ConstructorArguments.FirstOrDefault().Value;
+                    System.Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
