df8ed3b79e53751ac542e92713ba389c25e0710f
Николай Дойчев <nikolay.doichev@gmail.com>
2019-10-05 16:54:17 +0300

    08.ExceptionAndErrorHandling

---
+++ 08.ExceptionsAndErrorHandling/08.ExceptionsAndErrorHandling/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace _08.ExceptionsAndErrorHandling
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
